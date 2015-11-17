0f5701f247dfff5a7e5f5e60e986b267b70b86e2
Michael Cremer <cremerm@hochschule-trier.de>
2015-11-17 17:56:51 +0100

    ersten Handler hinzufügen

--- userwebbackendservice.go
+++ userwebbackendservice.go
@@ -20,20 +20,27 @@ func main() {
 	//serviceclient.Configure(cacheadapter.New("discovery:8500", 10*time.Second, 5*time.Second, 3))
 	servicecache.Start(3, 5*time.Second)
 	log.Println("listening on 8000")
+
+	r.Methods("GET").
+		Path("/login").
+		Handler(createProxy(
+		"authentication-service",
+		"/oauth/token"))
+
 	// Bind to a port and pass our router in
 	http.ListenAndServe(":8000", r)
 }
 
 func createProxy(service, servicePath string) http.Handler {
 	address, _ := servicecache.GetServiceAddress(service)
-	targetUrl, err := url.Parse(fmt.Sprintf("http://%s%s", address, servicePath))
+	targetURL, err := url.Parse(fmt.Sprintf("http://%s%s", address, servicePath))
 	if err != nil {
 		panic(err)
 	}
-	handler := httputil.NewSingleHostReverseProxy(targetUrl)
+	handler := httputil.NewSingleHostReverseProxy(targetURL)
 	handler.Director = func(r *http.Request) {
 		r.Host = address
-		r.URL = targetUrl
+		r.URL = targetURL
 	}
 	return handler
 }
