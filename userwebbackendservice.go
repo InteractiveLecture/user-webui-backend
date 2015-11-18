576e6545eb6a51cd0d435f643743a22fd1b36baa
Michael Cremer <cremerm@hochschule-trier.de>
2015-11-18 14:36:16 +0100

    Topics Handler: jwtware und proxy hinzufügen

--- userwebbackendservice.go
+++ userwebbackendservice.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"time"
 
+	"github.com/InteractiveLecture/middlewares/jwtware"
 	"github.com/InteractiveLecture/servicecache"
 	"github.com/gorilla/mux"
 )
@@ -21,11 +22,108 @@ func main() {
 	servicecache.Start(3, 5*time.Second)
 	log.Println("listening on 8000")
 
-	r.Methods("GET").
+	// Posten der Login-daten
+	r.Methods("POST").
 		Path("/login").
 		Handler(createProxy(
 		"authentication-service",
 		"/oauth/token"))
+
+	// TOPIC Anfragen
+	//---------------------
+
+	// TODO Authware und Groupware Nutzung bestimmen
+
+	// Liste aller Topics
+	r.Methods("GET").
+		Path("/topics").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics")))
+
+	// Erzeuge ein neues Topic
+	r.Methods("POST").
+		Path("/topics").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics")))
+
+	// Ein spezielles Topic nachfragen
+	r.Methods("GET").
+		Path("/topics/{id}").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}")))
+
+	// Verändert ein vorhandenes Topic
+	r.Methods("PUT").
+		Path("/topics/{id}").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}")))
+
+	// Löscht ein vorhandenes Topic
+	r.Methods("DELETE").
+		Path("/topics/{id}").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}")))
+
+	// Erfragt ein neues Rootmodul
+	r.Methods("GET").
+		Path("/topics/{id}/modules").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}/modules")))
+
+	// Erzeugt ein neues Rootmodul
+	r.Methods("POST").
+		Path("/topics/{id}/modules").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}/modules")))
+
+	// Liste aller officer eines Topics anfordern
+	r.Methods("GET").
+		Path("/topics/{id}/officers").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}/officers")))
+
+	// Einen officer für ein Topic löschen
+	r.Methods("DELETE").
+		Path("/topics/{id}/officers/{userId}").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}/officers/{userId}")))
+
+	// Einen officer einem Topic hinfügen
+	r.Methods("POST").
+		Path("/topics/{id}/officers").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}/officers")))
+
+	// Liste aller assistant eines Topics anfordern
+	r.Methods("GET").
+		Path("/topics/{id}/assistants").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}/assistants")))
+
+	// Einen assistant für ein Topic löschen
+	r.Methods("DELETE").
+		Path("/topics/{id}/assistants/{userId}").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}/assistants/{userId}")))
+
+	// Einen assistant einem Topic hinfügen
+	r.Methods("POST").
+		Path("/topics/{id}/assistants").
+		Handler(jwtware.New(createProxy(
+		"lecture-service",
+		"/topics/{id}/assistants")))
 
 	// Bind to a port and pass our router in
 	http.ListenAndServe(":8000", r)
